2eadcf92dce1c284da736b8bbfe343b26673a51a
cahuizar <hire@carloshuizar.com>
2017-01-28 18:50:56 -0500

    bacsic initialize

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 test = [7,7,2,6,5,3,6,1]
 
+queens = []
+
 conflict = 0;
 
 #create conflict function
 
 def main():
-    NorthEastConflict(1)
-    print(conflict)
+    Initialize()
 
 def Initialize()
     column_conflict = []
-    for(int x =0; x < 8; x++):
-        column_conflict[x] = InitConflict()
+    row = 0;
+    #for(int row =0; row < 8; row++):
+    column_conflict[row] = InitConflict(row)
         
-
-def InitConflict()
+def InitConflict(row)
+    conflict = 0
+    conflict = NorthWest(row, conflict)
+    conflict = West(row, conflict)
+    conflict = SouthWest(row, conflict)
+    return conflict    
     
-    
-
-def NorthWestConflict(cur_index):
-    global conflict
+def NorthWestConflict(cur_index, conflict):
     print("Searching for conflicts on NorthWestConflict")
     index_counter = cur_index
     column_value = test[cur_index]
@@ -31,9 +34,9 @@ def NorthWestConflict(cur_index):
             print("Actual Column value: ",column_value)
             print("Col val: ",test[index_counter])
             conflict += 1
+    return conflict
 
-def WestConflict(cur_index):
-    global conflict
+def WestConflict(cur_index, conflict):
     print("Searching for conflicts on West")
     conflict = 0;
     index_counter = cur_index
@@ -43,10 +46,10 @@ def WestConflict(cur_index):
         if(test[cur_index] == test[index_counter]):
             print("Same on column: ",index_counter)
             conflict += 1
+    return conflict
         
 
-def SouthWestConflict(cur_index):
-    global conflict
+def SouthWestConflict(cur_index, conflict):
     print("Searching for conflicts on SouthWest")
     index_counter = cur_index
     column_value = test[cur_index]
@@ -58,9 +61,9 @@ def SouthWestConflict(cur_index):
             print("Actual Column value: ",column_value)
             print("Col val: ",test[index_counter])
             conflict += 1
+    return conflict
 
-def SouthEastConflict(cur_index):
-    global conflict
+def SouthEastConflict(cur_index, conflict):
     print("Searching for conflicts on SouthEast")
     index_counter = cur_index
     column_value = test[cur_index]
@@ -72,9 +75,9 @@ def SouthEastConflict(cur_index):
             print("Actual Column value: ",column_value)
             print("Col val: ",test[index_counter])
             conflict += 1
+    return conflict
 
-def EastConflict(cur_index):
-    global conflict
+def EastConflict(cur_index, conflict):
     print("Searching for conflicts on East")
     conflict = 0;
     index_counter = cur_index
@@ -83,9 +86,10 @@ def EastConflict(cur_index):
         index_counter += 1
         if(test[cur_index] == test[index_counter]):
             print("Same on column: ",index_counter)
-            conflict += 1           
+            conflict += 1
+    return conflict
 
-def NorthEastConflict(cur_index):
+def NorthEastConflict(cur_index, conflict):
     global conflict
     print("Searching for conflicts on NorthEastConflict")
     index_counter = cur_index
@@ -97,6 +101,7 @@ def NorthEastConflict(cur_index):
         if( column_value == test[index_counter]):
             print("Actual Column value: ",column_value)
             print("Col val: ",test[index_counter])
-            conflict += 1    
+            conflict += 1
+    return conflict
 
 main();
